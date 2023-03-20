641616674cc92e58bb1f7d0979fceb3f10801756
SrtaJulie <90550949+SrtaJulie@users.noreply.github.com>
2023-03-20 09:52:15 +0100

    Page OK

--- pages/rechercheClient.php
+++ pages/rechercheClient.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="Fr">
   <head>
-      <title>Dolistats - Recherche client</title>
+      <title>Dolistats - Recherche clients</title>
       <meta charset="utf-8">
 	  <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
 	  <link rel="stylesheet" href="../fontawesome-free-6.2.1-web/css/all.css">
@@ -14,8 +14,8 @@
 		<div class="row"></div>
 		<div class="row col-xs-12">
 			<div class="col-xs-5">
-                <div class="col-xs-2"><a href="rechercherArticle.php"><img class="logoNav" src="../assets/RechercheArticleMenu.png" alt="logo Recherche Articles"></a></div>
-				<div class="col-xs-2"><img class="logoNav" src="../assets/RechercheClientMenu.png" alt="logo Recherche clients"></div>
+                <div class="col-xs-2"><a href="rechercheArticle.php"><img class="logoNav" src="../assets/RechercheArticleMenu.png" alt="logo Recherche Articles"></a></div>
+				<div class="col-xs-2"><a href="rechercheClient.php"><img class="logoNav" src="../assets/RechercheClientMenu.png" alt="logo Recherche clients"></a></div>
                 <div class="col-xs-2"><img class="logoNav" src="../assets/PalmaresArticlesMenu.png" alt="logo palmares articles"></div>
 				<div class="col-xs-2"><img class="logoNav" src="../assets/PalmaresClientMenu.png" alt="logo palmares client"></div>
                 <div class="col-xs-2"><img class="logoNav" src="../assets/ComparaisonCAMenu.png" alt="logo CA"></div>
@@ -27,7 +27,7 @@
 				<form action="rechercheClient.php" method="post">				
 					<div class="col-xs-7"> Nom Prénom
 					<button type="submit" name="deconnexion" value="true" title="Déconnexion">Déconnexion</button> </div>
-                    <div class="col-xs-1"><img class="logoNav" src="../assets/Logo.png" alt="logo Doli"></div>
+                    <div class="col-xs-5"><img class="logoNav" src="../assets/Logo.png" alt="logo Doli"></div>
 				</form>
 			</div>	
 		</div>
@@ -39,8 +39,8 @@
 		<!-- Recherche par critères -->
 		<div class="row">
             <div class="search-box">
-                <form action="index.php" method="post">
-                    <input type="search" name="recherche" id="search-input" placeholder="Libellé de l'article" required value="<?php 
+                <form action="rechercheClient.php" method="post">
+                    <input type="search" name="recherche" id="search-input" placeholder="Nom du client" required value="<?php 
 					if(isset($_POST['rechercheNom'])) {
 						echo $_POST['rechercheNom'];
 					} else {
