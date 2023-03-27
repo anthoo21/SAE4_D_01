640c2312642ff4b39fd3f0d9c8a4bcc17e5060b5
SrtaJulie <90550949+SrtaJulie@users.noreply.github.com>
2023-03-27 09:56:33 +0200

    lien style OK + retrait commentaires

--- dolistats/views/palmaresClient.php
+++ dolistats/views/palmaresClient.php
@@ -5,7 +5,7 @@
       <meta charset="utf-8">
 	  <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
 	  <link rel="stylesheet" href="../fontawesome-free-5.10.2-web/css/all.css">
-	  <link rel="stylesheet" href="../css/styleFacture.css"> 
+	  <link rel="stylesheet" href="../css/stylePalmares.css"> 
   </head>
   
   <body>
@@ -17,8 +17,8 @@
                 <form action="index.php" method="post">
                     <div class="col-xs-2">
                         <input type="hidden" name="controller" value="Articles">
-                        <!-- <input type="hidden" name="apiUrl" value="<?php //echo $apiUrl;?>">
-                        <input type="hidden" name="apiKey" value="<?php //echo $apiKey;?>"> -->
+                        <input type="hidden" name="apiUrl" value="<?php echo $apiUrl;?>">
+                        <input type="hidden" name="apiKey" value="<?php echo $apiKey;?>">
                         <button type="submit" class="boutonNavbar"><img class="logoNav" src="../assets/RechercheArticleMenu.png"
                         alt="logo Recherche Articles"></button>
                     </div>
@@ -26,8 +26,8 @@
                 <form action="index.php" method="post">
                     <div class="col-xs-2">
                         <input type="hidden" name="controller" value="Clients">
-                        <!-- <input type="hidden" name="apiUrl" value="<?php //echo $apiUrl;?>">
-                        <input type="hidden" name="apiKey" value="<?php //echo $apiKey;?>"> -->
+                        <input type="hidden" name="apiUrl" value="<?php echo $apiUrl;?>">
+                        <input type="hidden" name="apiKey" value="<?php echo $apiKey;?>">
                         <button type="submit" class="boutonNavbar"><img class="logoNav" src="../assets/RechercheClientMenu.png"
                         alt="logo Recherche clients"></button>
                     </div>
@@ -35,8 +35,8 @@
                 <form action="index.php" method="post">
                     <div class="col-xs-2">
                         <input type="hidden" name="controller" value="PalmaresArticles">
-                        <!-- <input type="hidden" name="apiUrl" value="<?php //echo $apiUrl;?>">
-                        <input type="hidden" name="apiKey" value="<?php //echo $apiKey;?>"> -->
+                        <input type="hidden" name="apiUrl" value="<?php echo $apiUrl;?>">
+                        <input type="hidden" name="apiKey" value="<?php echo $apiKey;?>">
                         <button type="submit" class="boutonNavbar"><img class="logoNav" src="../assets/PalmaresArticlesMenu.png" 
                         alt="logo palmares articles"></button>
                     </div>
@@ -44,8 +44,8 @@
                 <form action="index.php" method="post">
                     <div class="col-xs-2">
                         <input type="hidden" name="controller" value="PalmaresClients">
-                        <!-- <input type="hidden" name="apiUrl" value="<?php //echo $apiUrl;?>">
-                        <input type="hidden" name="apiKey" value="<?php //echo $apiKey;?>"> -->
+                        <input type="hidden" name="apiUrl" value="<?php echo $apiUrl;?>">
+                        <input type="hidden" name="apiKey" value="<?php echo $apiKey;?>">
                         <button type="submit" class="boutonNavbar"><img class="logoNav" src="../assets/PalmaresClientMenu.png" 
                         alt="logo palmares client"></button>
                     </div>
@@ -53,8 +53,8 @@
                 <form action="index.php" method="post">
                     <div class="col-xs-2">
                         <input type="hidden" name="controller" value="CA">
-                        <!-- <input type="hidden" name="apiUrl" value="<?php //echo $apiUrl;?>">
-                        <input type="hidden" name="apiKey" value="<?php //echo $apiKey;?>"> -->
+                        <input type="hidden" name="apiUrl" value="<?php echo $apiUrl;?>">
+                        <input type="hidden" name="apiKey" value="<?php echo $apiKey;?>">
                         <button type="submit" class="boutonNavbar"><img class="logoNav" src="../assets/ComparaisonCAMenu.png" 
                         alt="logo CA"></button>
                     </div>
@@ -64,7 +64,7 @@
 			<!--Espace dans la navbar-->
 			</div>
 			<div class="col-xs-2">
-				<form action="palmaresClient.php" method="post">				
+				<form action="rechercheArticle.php" method="post">				
 					<div class="col-xs-7"> Nom Prénom
 					<button type="submit" name="deconnexion" value="true" title="Déconnexion">Déconnexion</button> </div>
                     <div class="col-xs-5"><img class="logoNav" src="../assets/Logo.png" alt="logo Doli"></div>
