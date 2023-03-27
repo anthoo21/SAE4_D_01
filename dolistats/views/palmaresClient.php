cc679cdf0b83071af8c300cffbdf23edf7e76f34
SrtaJulie <90550949+SrtaJulie@users.noreply.github.com>
2023-03-27 09:54:00 +0200

    ajout du squelette de la page

---
+++ dolistats/views/palmaresClient.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="Fr">
+  <head>
+      <title>Dolistats - Palmarès client</title>
+      <meta charset="utf-8">
+	  <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+	  <link rel="stylesheet" href="../fontawesome-free-5.10.2-web/css/all.css">
+	  <link rel="stylesheet" href="../css/styleFacture.css"> 
+  </head>
+  
+  <body>
+  <div class="container nav">
+		<!-- Nav-bar -->
+		<div class="row"></div>
+		<div class="row col-xs-12">
+			<div class="col-xs-5">
+                <form action="index.php" method="post">
+                    <div class="col-xs-2">
+                        <input type="hidden" name="controller" value="Articles">
+                        <!-- <input type="hidden" name="apiUrl" value="<?php //echo $apiUrl;?>">
+                        <input type="hidden" name="apiKey" value="<?php //echo $apiKey;?>"> -->
+                        <button type="submit" class="boutonNavbar"><img class="logoNav" src="../assets/RechercheArticleMenu.png"
+                        alt="logo Recherche Articles"></button>
+                    </div>
+				</form>
+                <form action="index.php" method="post">
+                    <div class="col-xs-2">
+                        <input type="hidden" name="controller" value="Clients">
+                        <!-- <input type="hidden" name="apiUrl" value="<?php //echo $apiUrl;?>">
+                        <input type="hidden" name="apiKey" value="<?php //echo $apiKey;?>"> -->
+                        <button type="submit" class="boutonNavbar"><img class="logoNav" src="../assets/RechercheClientMenu.png"
+                        alt="logo Recherche clients"></button>
+                    </div>
+                </form>
+                <form action="index.php" method="post">
+                    <div class="col-xs-2">
+                        <input type="hidden" name="controller" value="PalmaresArticles">
+                        <!-- <input type="hidden" name="apiUrl" value="<?php //echo $apiUrl;?>">
+                        <input type="hidden" name="apiKey" value="<?php //echo $apiKey;?>"> -->
+                        <button type="submit" class="boutonNavbar"><img class="logoNav" src="../assets/PalmaresArticlesMenu.png" 
+                        alt="logo palmares articles"></button>
+                    </div>
+				</form>
+                <form action="index.php" method="post">
+                    <div class="col-xs-2">
+                        <input type="hidden" name="controller" value="PalmaresClients">
+                        <!-- <input type="hidden" name="apiUrl" value="<?php //echo $apiUrl;?>">
+                        <input type="hidden" name="apiKey" value="<?php //echo $apiKey;?>"> -->
+                        <button type="submit" class="boutonNavbar"><img class="logoNav" src="../assets/PalmaresClientMenu.png" 
+                        alt="logo palmares client"></button>
+                    </div>
+                </form>
+                <form action="index.php" method="post">
+                    <div class="col-xs-2">
+                        <input type="hidden" name="controller" value="CA">
+                        <!-- <input type="hidden" name="apiUrl" value="<?php //echo $apiUrl;?>">
+                        <input type="hidden" name="apiKey" value="<?php //echo $apiKey;?>"> -->
+                        <button type="submit" class="boutonNavbar"><img class="logoNav" src="../assets/ComparaisonCAMenu.png" 
+                        alt="logo CA"></button>
+                    </div>
+                </form>
+			</div>	
+			<div class="col-xs-5">
+			<!--Espace dans la navbar-->
+			</div>
+			<div class="col-xs-2">
+				<form action="palmaresClient.php" method="post">				
+					<div class="col-xs-7"> Nom Prénom
+					<button type="submit" name="deconnexion" value="true" title="Déconnexion">Déconnexion</button> </div>
+                    <div class="col-xs-5"><img class="logoNav" src="../assets/Logo.png" alt="logo Doli"></div>
+				</form>
+			</div>	
+		</div>
+    </div>
+    <div class="container ">
+		<div class="row">
+            <p class="titre">Palmarès clients</p>
+        </div>
+	</div>
+  </body>
+</html>
