ba4c3fc49311a551c89c36f3aee1a965dec0fd35
Jeff7738 <jean-francois.froment@iut-rodez.fr>
2023-03-28 14:00:10 +0200

    continuation graphique

--- dolistats/views/palmaresarticle.php
+++ dolistats/views/palmaresarticle.php
@@ -128,82 +128,41 @@
     </form>
         <?php
 
-var_dump($resultat);
-// TODO pourquoi j'ai pas tous 
+//var_dump($resultat);
+$Donneequantite = [];
+$Donneelibelle = [];
+//$ligne
 foreach ($resultat as $ligne) {
     foreach ($ligne['lines'] as $wanted) {
-        echo "<tr>";
-        echo "<td>" . $wanted['qty'] . "</td>";
-        echo "<td>" . $wanted['libelle'] . "</td>";
-        echo "</tr>";
+        $wanted['qty'].',';
+        $wanted['libelle'];
+        //echo $wanted['ref']."<br>";
     }
 }
-?> <!-- 
+var_dump($Donneequantite);
+?> 
 <div class="row">
     <div class="col-xs-3 "></div>
     <div class="col-xs-6">
         <h1>Exemple Diagramme Barres</h1>
         <canvas id="myChart" width="400" height="225"></canvas>
     </div>
-</div> -->
+</div>
 
-<?php /*
-function CouleurAleatoire() {
-    // Retourne un code couleur aléatoire
-    $a = DecHex(mt_rand(0,15)); 
-    $b = DecHex(mt_rand(0,15)); 
-    $c = DecHex(mt_rand(0,15)); 
-    $d = DecHex(mt_rand(0,15)); 
-    $e = DecHex(mt_rand(0,15)); 
-    $f = DecHex(mt_rand(0,15)); 
-    $hexac = $a . $b . $c . $d . $e . $f;
-    return "#".$hexac;
-}
-
-$leTitreBarChart="Population mondiale en 2050";
-$mesDonneesBarChart=[];
-$mesDonneesBarChart['Africa']=mt_rand(0,9000);
-$mesDonneesBarChart['Asia']=mt_rand(0,9000);
-$mesDonneesBarChart['Europe']=mt_rand(0,9000);
-$mesDonneesBarChart['Amérique du Sud']=mt_rand(0,9000);
-$mesDonneesBarChart['Amérique du Nord']=mt_rand(0,9000);
-
-
-$laChaineBarChartLabel="" ; 
-$laChaineBarChartValeurs="" ;
-$laChaineBarChartCouleurs="" ;
-
-foreach($mesDonneesBarChart as $region=>$valeur) {
-    if ($laChaineBarChartLabel!="") $laChaineBarChartLabel.=",";
-    $laChaineBarChartLabel.='"'.$region.'"' ;
-    
-    if ($laChaineBarChartValeurs!="") $laChaineBarChartValeurs.=",";
-    $laChaineBarChartValeurs.='"'.$valeur.'"' ;
-    
-    if ($laChaineBarChartCouleurs!="") $laChaineBarChartCouleurs.=",";
-    $laChaineBarChartCouleurs.='"'.CouleurAleatoire().'"' ;
-}
-$laChaineBarChartLabel="[".$laChaineBarChartLabel."]";  // Ajout des crochets
-$laChaineBarChartValeurs="[".$laChaineBarChartValeurs."]";  // Ajout des crochets
-$laChaineBarChartCouleurs="[".$laChaineBarChartCouleurs."]";  // Ajout des crochets
-
-
-// var_dump($laChaineBarChartLabel);
-// var_dump($laChaineBarChartValeurs);
-// var_dump($laChaineBarChartCouleurs);
-
+<?php 
+    $LeTitre = "Produits le plus vendue en quantité"
 ?>
 
 <script type="text/javascript" src="../jchart4-2-1-Min.js"></script>
 <script>
 // setup 
 const data = {
-    labels: <?php echo $laChaineBarChartLabel; ?>, // TODO tous les libelles des produits
+    labels: <?php echo $Donneelibelle; ?>, // TODO tous les libelles des produits
     
     datasets: [{
-        label: '<?php echo $leTitreBarChart; ?>', // TODO le titre
-        data: "<?php echo $laChaineBarChartValeurs; ?>", // TODO les quantités
-        backgroundColor: <?php echo $laChaineBarChartCouleurs; ?>, // TODO le style
+        label: '<?php echo $LeTitre; ?>', // TODO le titre
+        data: "<?php echo $Donneequantite; ?>", // TODO les quantités
+        backgroundColor:  Utils.transparentize(Utils.CHART_COLORS.red, 0.5), // TODO le style
    
         borderWidth: 1
   }]
@@ -226,8 +185,7 @@ const config1 = {
   }
 };	
 const myChart = new Chart(document.getElementById('myChart'),config1);
-</script> */
-?>
+</script> 
     </div>
 </body>
 
