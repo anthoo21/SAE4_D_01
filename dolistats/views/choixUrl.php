8cfab4aef4a0a6ea7431cba06df8c148d8bb4014
SrtaJulie <90550949+SrtaJulie@users.noreply.github.com>
2023-03-28 13:56:11 +0200

    amélioration style

--- dolistats/views/choixUrl.php
+++ dolistats/views/choixUrl.php
@@ -5,11 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../fontawesome-free-5.10.2-web/css/all.css">
-    <link rel="stylesheet" href="../css/choixUrl.css"> 
+    <link rel="stylesheet" href="../css/styleUrl.css"> 
     <title>Document</title>
 </head>
 <body>
-    <div class="container">
+    <!--Image accueil-->
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="logoDoli">
+                <img class="logo" src="../assets/Logo.png" alt="Logo Doli">
+            </div>
+        </div>	
+    </div>
+    <div class="container menu">
+        
+        <div></br></br></br></div>
         <?php 
         if(isset($error)) {
             ?> 
@@ -27,9 +37,10 @@
                 <button type="submit">Valider</button><br/>
             </form>
         </div>
-        <div class="row">
+        <div class="row url">
             <p>L'url doit être sous la forme : "http://dolibarr.iut-rodez.fr/G2022-XX/htdocs/api/index.php/"</p>
         </div>
+        <div></br></br></br></div>
     </div>
 </body>
-</html>
+</html
