364feead01c87c40a00e12a808766edb262386a8
Jeff7738 <jean-francois.froment@iut-rodez.fr>
2023-03-30 18:26:45 +0200

    fonctionnalité top fonctionne , tests nécessaire !

--- dolistats/views/palmaresarticle.php
+++ dolistats/views/palmaresarticle.php
@@ -94,7 +94,10 @@
                         <input type="date" name="dateDe" required>
                         à
                         <input type="date" name="dateA" required>
-                        <input type="hidden" name="controller" value="palamares">
+                        <input type="hidden" name="controller" value="PalmaresArticles">
+                        <input type="hidden" name="action" value="palmares">
+                        <input type="hidden" name="apiUrl" value="<?php echo $apiUrl; ?>">
+                        <input type="hidden" name="apiKey" value="<?php echo $apiKey; ?>">
                         <button type="submit" class="search-button">Valider</span></button>
                     </div>
                 </form>
@@ -119,17 +122,10 @@
                 ?>
             </table>
         </div>
-        <form action="index.php" method="post" class="form">
-            <input type="hidden" name="controller" value="PalmaresArticles">
-            <input type="hidden" name="action" value="palmares">
-            <input type="hidden" name="apiUrl" value="<?php echo $apiUrl; ?>">
-            <input type="hidden" name="apiKey" value="<?php echo $apiKey; ?>">
-            <button type="submit" class="button submit"></i></button>
-        </form>
         <div class="row">
             <div class="col-xs-3 "></div>
             <div class="col-xs-6">
-                <h1>Exemple Diagramme Barres</h1>
+                <h1>Produits le plus vendue en quantité</h1>
                 <canvas id="myChart" width="400" height="225"></canvas>
             </div>
         </div>
@@ -138,8 +134,7 @@
         $Donnees = [];
         $ligneLibelle = "";
         $ligneQuantite = "";
-        $LeTitre = "Produits le plus vendue en quantité";
-        $sommeqty = 0;
+        $LeTitre = "Quantité de produits";
         foreach ($resultat as $ligne) {
             foreach ($ligne['lines'] as $wanted) {
                 if (array_key_exists($wanted['libelle'], $Donnees)) {
@@ -149,46 +144,61 @@
                 }
             }
         }
-        foreach($Donnees as $label=>$quantite) {
-            if ($ligneLibelle!="") $ligneLibelle.=",";
-            $ligneLibelle.='"'.$label.'"' ;
-            
-            if ($ligneQuantite!="") $ligneQuantite.=",";
-            $ligneQuantite.='"'.$quantite.'"' ;
+        /* Top , 
+        TODO gérer qaund c'est 10 et qu'il y a pas 10 produit (le break)
+            nb négatif
+            0 affiche le graph mais se passe rien
+        */
+        if (isset(($_POST['rechercheNb']))) {
+            while (count($Donnees) != $_POST['rechercheNb']) { 
+                if(count($Donnees) == 0) {
+                    break; 
+                }
+                $min = array_search(min($Donnees), $Donnees);
+                unset($Donnees[$min]);
+            }
+        }
+        // Mise en forme
+        foreach ($Donnees as $label => $quantite) {
+            if ($ligneLibelle != "") $ligneLibelle .= ",";
+            $ligneLibelle .= '"' . $label . '"';
+
+            if ($ligneQuantite != "") $ligneQuantite .= ",";
+            $ligneQuantite .= '"' . $quantite . '"';
         }
         $ligneLibelle = "[" . $ligneLibelle . "]";  // Ajout des crochets
         $ligneQuantite = "[" . $ligneQuantite . "]";  // Ajout des crochets
         ?>
-    </div>  
+    </div>
     <script type="text/javascript" src="../jchart4-2-1-Min.js"></script>
-        <script>
-            // setup 
-            const data = {
-                labels: <?php echo $ligneLibelle; ?>, 
+    <script>
+        // setup 
+        const data = {
+            labels: <?php echo $ligneLibelle; ?>,
 
-                datasets: [{
-                    label: '<?php echo $LeTitre; ?>', 
-                    data: <?php echo $ligneQuantite; ?>, 
-                    backgroundColor: 'rgb(255, 99, 132)', // TODO le style
+            datasets: [{
+                label: '<?php echo $LeTitre; ?>',
+                data: <?php echo $ligneQuantite; ?>,
+                backgroundColor: 'rgb(255, 99, 132)', // TODO le style
 
-                    borderWidth: 1
-                }]
-            };
-            console.log(data);
+                borderWidth: 1
+            }]
+        };
+        console.log(data);
 
-            const config1 = {
-                type: 'bar',
-                data,
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true
-                        }
+        const config1 = {
+            type: 'bar',
+            data,
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
                     }
                 }
-            };
-            const myChart = new Chart(document.getElementById('myChart'), config1);
-        </script>
+            }
+        };
+        const myChart = new Chart(document.getElementById('myChart'), config1);
+    </script>
 </body>
 
 </html>
